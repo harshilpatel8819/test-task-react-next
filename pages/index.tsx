b00554b2f647bc18fecc980e1381a0dbba31196d
harshilpatel <harshilpatel8819@gmail.com>
2022-02-26 09:13:19 +0530

    section

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import Card from "components/Card";
 
 import { NextPage } from "next/types";
 import Image from "next/image";
+import TabPane from "components/TabPane";
 
 const Home: NextPage = () => {
   return (
@@ -58,8 +59,28 @@ const Home: NextPage = () => {
               </Button>
             </Card>
           </Grid>
+          <Grid item xs={12}>
+            <Card className={styles.nftCard}>
+              <Typography className={styles.nftHeading}>
+                Start <br /> Creating your
+                <br /> NFT Today
+              </Typography>
+              <Button className={styles.nftCardButton} variant="contained">
+                Create NFT
+                <Link passHref href="/">
+                  <Image
+                    src="/images/ArrowWhite.svg"
+                    width={16}
+                    height={16}
+                    alt="arrow"
+                  />
+                </Link>
+              </Button>
+            </Card>
+          </Grid>
         </Grid>
       </Container>
+      <TabPane />
     </Layout>
   );
 };
